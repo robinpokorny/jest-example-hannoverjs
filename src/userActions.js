1c70d65bc147380278d4138b24ae3c27f9b5b875
Robin Pokorny <me@robinpokorny.com>
2017-03-14 17:47:04 +0100

    :zap: Use withExtraArgument

--- src/userActions.js
+++ src/userActions.js
@@ -1,7 +1,19 @@
 export const LOAD_USER_BOOKINGS = 'LOAD_USER_BOOKINGS'
 
+// Expects that `fetch` is injected using `extraArgument`
+// while creating the store:
+//   const store = createStore(
+//     reducer,
+//     applyMiddleware(thunk.withExtraArgument({ fetch }))
+//   )
+// Read more:
+// https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
+//
+// The created action will be evaluated by redux-promise-middleware:
+// https://github.com/pburtchaell/redux-promise-middleware
+
 export const loadUserBookings = () =>
-  ({ getState, fetch }) => {
+  (dispatch, getState, { fetch }) => {
     const { name } = getState()
 
     const promise = fetch(`${name}/bookings`)
@@ -9,7 +21,7 @@ export const loadUserBookings = () =>
         response.json()
       )
 
-    return ({
+    dispatch({
       type: LOAD_USER_BOOKINGS,
       payload: {
         data: name,
--- src/userActions.spec.js
+++ src/userActions.spec.js
@@ -5,30 +5,38 @@ import * as actions from './userActions'
 describe('loadUserBookings', () => {
   const action = actions.loadUserBookings()
 
-  const state = {
+  const dispatch = jest.fn()
+  const getState = () => ({
     name: 'gregor'
-  }
+  })
   const deps = {
     fetch: jest.fn(() => Promise.resolve({
       json: () => {}
-    })),
-    getState: () => state
+    }))
   }
 
   beforeEach(() => {
+    dispatch.mockClear()
     deps.fetch.mockClear()
   })
 
   test('is a valid FSA', () => {
-    expect(isFSA(action(deps))).toBe(true)
+    action(dispatch, getState, deps)
+
+    expect(dispatch).toHaveBeenCalledTimes(1)
+    const dispatchedAction = dispatch.mock.calls[0][0]
+
+    expect(isFSA(dispatchedAction)).toBe(true)
   })
 
   test('has correct data', () => {
-    expect(action(deps)).toMatchSnapshot()
+    action(dispatch, getState, deps)
+
+    expect(dispatch.mock.calls).toMatchSnapshot()
   })
 
   test('fetches bookings from the server', () => {
-    action(deps)
+    action(dispatch, getState, deps)
 
     expect(deps.fetch).toHaveBeenCalledTimes(1)
     expect(deps.fetch.mock.calls).toMatchSnapshot()
